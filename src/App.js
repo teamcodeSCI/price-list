82717be98801bb4e008460ce4700df2724ce8129
doanduc2000 <doanminhducvip2000@gmail.com>
2023-04-19 15:06:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from './layouts/Home';
+import { APP_URL } from './utils/const';
 
 function App() {
   return (
     <Routes>
-      <Route path={`/login`} element={''} />
-      <Route path={`/`} element={<Home />} />
-      <Route path={'*'} element={<Navigate to={`/`} />} />
+      <Route path={APP_URL} element={<Home />} />
+      <Route path={'*'} element={<Navigate to={APP_URL} />} />
     </Routes>
   );
 }
--- src/utils/const.js
+++ src/utils/const.js
@@ -1,2 +1,3 @@
 // export const baseURL = 'https://app.scigroupvn.com/servey/servey-backend/public/api';
-export const baseURL = 'http://127.0.0.1:8000/api';
+export const API_URL = 'http://127.0.0.1:8000/api';
+export const APP_URL = '/price-list';
