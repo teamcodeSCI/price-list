3dfb3897e33ada26446bf5c042288a1cfe375ddb
doanduc2000 <doanminhducvip2000@gmail.com>
2023-04-19 13:51:55 +0700

    first commit

--- src/components/PriceList/index.js
+++ src/components/PriceList/index.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import './priceList.scss';
 import closeIcon from '../../assets/icons/close-icon.svg';
 import { useOutside } from '../../utils/help';
@@ -6,6 +6,25 @@ import PriceItem from '../PriceItem';
 
 const PriceList = ({ handlePriceList }) => {
   const modalRef = useRef(null);
+  const [isAddPrice, setIsAddPrice] = useState(false);
+  const [addInfo, setAddInfo] = useState({
+    name: '',
+    price: '',
+    percent: '',
+    discount: '',
+    promotion: '',
+    description: '',
+  });
+  const count = (addInfo.price * (100 - addInfo.percent)) / 100;
+  const handleEditInfo = (e) => {
+    setAddInfo({
+      ...addInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleIsAddPrice = () => {
+    setIsAddPrice(!isAddPrice);
+  };
   useOutside(modalRef, handlePriceList);
   return (
     <div className='priceList'>
@@ -41,15 +60,7 @@ const PriceList = ({ handlePriceList }) => {
             discount={(8000000 * (100 - 45)) / 100}
             promotion={'Tặng răng sứ Titan'}
             description={'7 năm'}
-          />{' '}
-          <PriceItem
-            name={'Dio Hàn Quốc - Tiêu chuẩn'}
-            price={8000000}
-            percent={45}
-            discount={(8000000 * (100 - 45)) / 100}
-            promotion={'Tặng răng sứ Titan'}
-            description={'7 năm'}
-          />{' '}
+          />
           <PriceItem
             name={'Dio Hàn Quốc - Tiêu chuẩn'}
             price={8000000}
@@ -58,6 +69,76 @@ const PriceList = ({ handlePriceList }) => {
             promotion={'Tặng răng sứ Titan'}
             description={'7 năm'}
           />
+          <PriceItem
+            name={'Dio Hàn Quốc - Tiêu chuẩn'}
+            price={8000000}
+            percent={45}
+            discount={(8000000 * (100 - 45)) / 100}
+            promotion={'Tặng răng sứ Titan'}
+            description={'7 năm'}
+          />
+          <div className='priceList__addItem' style={isAddPrice ? { padding: '10px 20px' } : {}}>
+            {isAddPrice ? (
+              <ul>
+                <li className='priceList__name'>
+                  <input
+                    type='text'
+                    name='name'
+                    placeholder='Nhập tên dịch vụ ...'
+                    value={addInfo.name}
+                    onChange={handleEditInfo}
+                  />
+                </li>
+                <li className='priceList__price'>
+                  <input
+                    type='number'
+                    name='price'
+                    placeholder='Nhập giá gốc ...'
+                    value={addInfo.price}
+                    onChange={handleEditInfo}
+                  />
+                </li>
+                <li className='priceList__percent'>
+                  <input
+                    type='number'
+                    name='percent'
+                    placeholder='Nhập % giảm ...'
+                    value={addInfo.percent}
+                    onChange={handleEditInfo}
+                  />
+                </li>
+                <li className='priceList__discount'>
+                  <input type='number' name='discount' value={count} onChange={handleEditInfo} disabled />
+                </li>
+                <li className='priceList__promotion'>
+                  <input
+                    type='text'
+                    name='promotion'
+                    placeholder='Nhập ưu đãi ...'
+                    value={addInfo.promotion}
+                    onChange={handleEditInfo}
+                  />
+                </li>
+                <li className='priceList__description'>
+                  <input
+                    type='text'
+                    name='description'
+                    placeholder='Nhập ghi chú ...'
+                    value={addInfo.description}
+                    onChange={handleEditInfo}
+                  />
+                </li>
+                <li className='priceList__action'>
+                  <button className='priceList__save'></button>
+                  <button className='priceList__cancel' onClick={handleIsAddPrice}></button>
+                </li>
+              </ul>
+            ) : (
+              <div className='priceList__addBtn'>
+                <button onClick={handleIsAddPrice}>+ Thêm mới</button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
