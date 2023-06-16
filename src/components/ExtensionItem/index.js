6fa03be35d632c36aa83577b55e7e17aa81c2003
doanduc2000 <doanminhducvip2000@gmail.com>
2023-06-16 15:13:55 +0700

    update

--- src/components/ExtensionItem/index.js
+++ src/components/ExtensionItem/index.js
@@ -1,11 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './extensionItem.scss';
 
 const ExtensionItem = (props) => {
+  const start = props.startDate.split('/');
+  const end = props.endDate.split('/');
+  const [isEditExtension, setIsEditExtension] = useState(false);
+  const [startDate, setStartDate] = useState({
+    startMM: start[0],
+    startDD: start[1],
+    startYYYY: start[2],
+  });
+  const [endDate, setEndDate] = useState({
+    endMM: end[0],
+    endDD: end[1],
+    endYYYY: end[2],
+  });
+  const handleEditExtension = () => {
+    setIsEditExtension(!isEditExtension);
+  };
+  const handleSetStartDate = (e) => {
+    setStartDate({ ...startDate, [e.target.name]: e.target.value });
+  };
+  const handleSetEndDate = (e) => {
+    setEndDate({ ...endDate, [e.target.name]: e.target.value });
+  };
   return (
     <div className='extensionItem'>
-      <div className='extensionItem__text'>{props.startDate}</div>
-      <div className='extensionItem__text'>{props.endDate}</div>
+      <div className='extensionItem__text'>
+        {isEditExtension ? (
+          <div className='extensionItem__input'>
+            <input
+              type='number'
+              name='startMM'
+              placeholder='mm'
+              value={startDate.startMM}
+              onChange={handleSetStartDate}
+            />
+            /
+            <input
+              type='number'
+              name='startDD'
+              placeholder='dd'
+              value={startDate.startDD}
+              onChange={handleSetStartDate}
+            />
+            /
+            <input
+              type='number'
+              name='startYYYY'
+              placeholder='yyyy'
+              value={startDate.startYYYY}
+              onChange={handleSetStartDate}
+            />
+          </div>
+        ) : (
+          props.startDate
+        )}
+      </div>
+      <div className='extensionItem__text'>
+        {isEditExtension ? (
+          <div className='extensionItem__input'>
+            <input type='number' name='endMM' placeholder='mm' value={endDate.endMM} onChange={handleSetEndDate} /> /
+            <input type='number' name='endDD' placeholder='dd' value={endDate.endDD} onChange={handleSetEndDate} /> /
+            <input
+              type='number'
+              name='endYYYY'
+              placeholder='yyyy'
+              value={endDate.endYYYY}
+              onChange={handleSetEndDate}
+            />
+          </div>
+        ) : (
+          props.endDate
+        )}
+      </div>
+      <div className='extensionItem__action'>
+        {isEditExtension ? (
+          <>
+            <button className='extensionItem__save'></button>
+            <button className='extensionItem__close' onClick={handleEditExtension}></button>
+          </>
+        ) : (
+          <>
+            <button className='extensionItem__edit' onClick={handleEditExtension}></button>
+            <button className='extensionItem__delete'></button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/ExtensionList/index.js
+++ src/components/ExtensionList/index.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import './extensionList.scss';
 import closeIcon from '../../assets/icons/close-icon.svg';
 import { useOutside } from '../../utils/help';
@@ -11,6 +11,29 @@ const data = [
 ];
 const ExtensionList = ({ handleOpenExtension }) => {
   const wrapperRef = useRef(null);
+
+  const [isAddNew, setIsAddNew] = useState(false);
+  const [startDate, setStartDate] = useState({
+    startMM: '',
+    startDD: '',
+    startYYYY: '',
+  });
+  const [endDate, setEndDate] = useState({
+    endMM: '',
+    endDD: '',
+    endYYYY: '',
+  });
+  const handleIsAddNew = () => {
+    setIsAddNew(!isAddNew);
+    setStartDate({ ...startDate, startMM: '', startDD: '', startYYYY: '' });
+    setEndDate({ ...endDate, endMM: '', endDD: '', endYYYY: '' });
+  };
+  const handleSetStartDate = (e) => {
+    setStartDate({ ...startDate, [e.target.name]: e.target.value });
+  };
+  const handleSetEndDate = (e) => {
+    setEndDate({ ...endDate, [e.target.name]: e.target.value });
+  };
   useOutside(wrapperRef, handleOpenExtension);
   return (
     <div className='extensionList'>
@@ -21,15 +44,73 @@ const ExtensionList = ({ handleOpenExtension }) => {
         <div className='extensionList__close' onClick={handleOpenExtension}>
           <img width='20' height='20' src={closeIcon} alt='' />
         </div>
+
         <div className='extensionList__head'>
           <div className='extensionList__title'>Ngày bắt đầu</div>
           <div className='extensionList__title'>Ngày kết thúc</div>
         </div>
+
         <div className='extensionList__body'>
           {data.map((item, idx) => (
             <ExtensionItem key={idx} {...item} />
           ))}
         </div>
+        {isAddNew ? (
+          <div class='extensionList__item'>
+            <div class='extensionList__text'>
+              <span>Ngày bắt đầu:</span>
+              <div class='extensionList__input'>
+                <input
+                  type='number'
+                  name='startMM'
+                  placeholder='mm'
+                  value={startDate.startMM}
+                  onChange={handleSetStartDate}
+                />
+                /
+                <input
+                  type='number'
+                  name='startDD'
+                  placeholder='dd'
+                  value={startDate.startDD}
+                  onChange={handleSetStartDate}
+                />
+                /
+                <input
+                  type='number'
+                  name='startYYYY'
+                  placeholder='yyyy'
+                  value={startDate.startYYYY}
+                  onChange={handleSetStartDate}
+                />
+              </div>
+            </div>
+            <div class='extensionList__text'>
+              <span>Ngày kết thúc:</span>
+              <div class='extensionList__input'>
+                <input type='number' name='endMM' placeholder='mm' value={endDate.endMM} onChange={handleSetEndDate} />
+                /
+                <input type='number' name='endDD' placeholder='dd' value={endDate.endDD} onChange={handleSetEndDate} />
+                /
+                <input
+                  type='number'
+                  name='endYYYY'
+                  placeholder='yyyy'
+                  value={endDate.endYYYY}
+                  onChange={handleSetEndDate}
+                />
+              </div>
+            </div>
+            <div class='extensionList__action'>
+              <button class='extensionList__save'></button>
+              <button class='extensionList__cancel' onClick={handleIsAddNew}></button>
+            </div>
+          </div>
+        ) : (
+          <div className='extensionList__addNew' onClick={handleIsAddNew}>
+            + Thêm mới
+          </div>
+        )}
       </div>
     </div>
   );
