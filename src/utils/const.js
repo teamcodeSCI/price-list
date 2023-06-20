fc10724c5598bd7a0d72de29be3b74460d84fefc
doanduc2000 <doanminhducvip2000@gmail.com>
2023-06-20 10:40:07 +0700

    update

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,3 +1,3 @@
-// export const API_URL = 'https://scigroup.com.vn/app/price-list/be/public/api';
-export const API_URL = 'http://10.195.0.171/project/price-list-be/public/api';
+export const API_URL = 'https://scigroup.com.vn/app/price-list/be/public/api';
+//export const API_URL = 'http://10.195.0.171/project/price-list-be/public/api';
 export const APP_URL = '';
